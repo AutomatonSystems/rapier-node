a4350dabf41f69ce98200a5b83d712e7bdc5dc35
Simon Baldwin <simon@automaton.systems>
2020-09-07 12:23:39 +0100

    Working example and readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # rapier-node
-Minimal example of rapier.js working in node
+Minimal example of rapier.js working in node to simulate a very basic scene.
+
+## Usage
+
+There are a few hacks to use the @dimforge/rapier2d package directly in node:
+
+* checkout repo
+* npm install
+* Change @dimforge/rapier2d package.json to allow node esmodule imports:
+
+	To enable node to load the package add the following key/values:
+	* `"main": "rapier2d.js"`
+	* `"type": "module"`
+*  Fix NPE in world.step():
+
+	There seems to be some trouble with how the wasm state gets initialized by node as calling world.step() throws a NPE from `__wbg_now_0aafc2276b5e8d61` (the function name will vary slightly between builds) in `rapier2d_bg.js`, it seems it's trying to read a time from a bad memory address... - however it seems fine to just use optional chaining to ignore it:
+
+		1521: `var ret = getObject(arg0).now();`
+	becomes
+
+		1521: `var ret = getObject(arg0)?.now();`
+	
+	This will probably do something *_horrible_* somewhere else in the code, but in the extremely limited demo here it doesn't seem to be causing trouble...
+
+## Running
+
+With those two issue fixed it is possible to run the code:
+
+`node --experimental-wasm-modules index.js`
+
+You should see console output showing the position of the two entities every tick with additional event logging when they impact and seperate.
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+import * as Rapier from "@dimforge/rapier2d";
+
+// create world and event queue
+let world = new Rapier.World(0, 0);
+let events = new Rapier.EventQueue(true);
+console.log(world, events);
+
+// create two entities on a collision course
+let e1 = makeEntity(-1, 0.1, 1, 0, 0.5);
+let e2 = makeEntity(1, 0, -1, 0, 0.5);
+
+// handles
+console.log(e1.handle(), e2.handle());
+
+// simulate 1 second
+for(let i = 0; i < 60; i++){
+	world.stepWithEvents(events);
+
+	events.drainContactEvents((handle1, handle2, contactStarted) => {
+        console.log("Contact between:", handle1, "and", handle2, ". Started:", contactStarted);
+	});
+
+	events.drainProximityEvents((handle1, handle2, prevProximity, newProximity) => {
+        console.log("Proximity between: ", handle1, "and", handle2, ". Previous proximity:", prevProximity, "new proximity: ", newProximity);
+	});
+	  
+	let t1 = e1.translation();
+	let t2 = e2.translation();
+	console.log(`(${t1.x},${t1.y}) (${t2.x},${t2.y})`);
+}
+
+console.log("done.");
+
+/**
+ *	Util function to make an rigid body entity 
+ * 
+ * @param {Number} x 
+ * @param {Number} y 
+ * @param {Number} vx 
+ * @param {Number} vy 
+ * @param {Number} r 
+ */
+function makeEntity(x,y,vx,vy,r){
+	let bodyDesc = new Rapier.RigidBodyDesc("dynamic");
+	bodyDesc.setTranslation(x, y);
+	let entity = world.createRigidBody(bodyDesc);
+	entity.createCollider(Rapier.ColliderDesc.ball(r));
+	entity.applyImpulse(new Rapier.Vector(vx,vy), true);
+	return entity;
+}
